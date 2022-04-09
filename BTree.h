7041ce372780aeb1f1c11358724d9d197d1b138f
Iznagi1 <92677983+Iznagi1@users.noreply.github.com>
2022-04-09 17:27:36 +0300

    Create BTree.h

---
+++ BTree.h
@@ -0,0 +1,14 @@
+//
+// Created by izanagi on 08.04.2022.
+//
+#ifndef BTREE_BTREE_H
+#define BTREE_BTREE_H
+typedef int bool_t;
+struct node_t;
+struct BTree_t;
+struct BTree_t* BTree_malloc();
+void BTree_add(struct BTree_t* BTree, int data);
+void BTree_print(struct BTree_t* BTree);
+bool_t BTree_find(struct BTree_t* BTree, int data);
+void BTree_delete(struct BTree_t* BTree, int data);
+#endif //BTREE_BTREE_H
